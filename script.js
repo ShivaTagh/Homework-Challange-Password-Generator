18a54305cf6cb3dd75bd32ffc1f53a80346fff64
ShivaTagh <127795324+ShivaTagh@users.noreply.github.com>
2023-04-04 09:32:10 -0700

    Add files

--- script.js
+++ script.js
@@ -1,14 +1,69 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+  // Assignment JavaScript Code
+  // Query selectors
+  var generateBtn = document.querySelector("#generate");
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
-}
+  // variables for all upper and lowercase letters, numbers, and characters)
+  var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  var lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var optionsVariable = "";
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+
+  // Write a function that generates a random password in the "text area" box
+  function writePassword() {
+    var randomString = "";
+    passwordText.value = password;
+
+  // Chose password criteria
+  var lowercase = confirm("Do you want your password to contain lowercase?");
+  var uppercase = confirm("Do you want your password to contain uppercase?");
+  var num = confirm("Do you want your password to contain numbers?");
+  var specialChar = confirm("Do you want your password to contain special characters?");
+  var passwordLength = prompt("Choose a password length of at least 8 characters and no more than 128 characters.");
+
+  // Write another function that checks if it's between 8 and 128 characters
+  if (passwordLength < 8 || passwordLength > 128) {
+    var passwordLength = prompt("Choose a password length of at least 8 characters and no more than 128 characters.");
+    alert("Your password is not between 8 characters and 128 characters. Please try again.");
+  }
+
+  // Ensure that if someone clicks cancel for all password criteria that it asks them to choose at least one
+  else if (!lowercase && !uppercase && !num && !specialChar) {
+    alert("Error, invalid character type.\nPlease include at least one type of character.");
+    var lowercase = confirm("Do you want your password to contain lowercase letters?");
+    var uppercase = confirm("Do you want your password to contain uppercase letters?");
+    var num = confirm("Do you want your password to contain numbers?");
+    var specialChar = confirm("Do you want your password to contain special characters?");
+  }
+
+  // Use the criteria that are selected to generate the password
+  if (lowercase) {
+    optionsVariable += lowercaseAlphabet;
+  }
+
+  if (uppercase) {
+    optionsVariable += uppercaseAlphabet;
+  }
+
+  if (num) {
+    optionsVariable += numbers;
+  }
+
+  if (specialChar) {
+    optionsVariable += specialCharacters;
+  }
+
+  // Generates a random password based on the criteria that have been selected
+  for (var i = 0; i < passwordLength; i++) {
+  // Continues to select a random character value from the string until it is the desired length
+    randomString += optionsVariable.charAt(Math.floor(Math.random() * optionsVariable.length));
+  }
+    passwordText.value = randomString;
+    var password = generatePassword();
+  }
+
